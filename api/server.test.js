f165aadc97bcafae20f1661f974d48254e83146d
RoseGabriely <rosegabriely14@gmail.com>
2021-11-12 19:30:39 -0500

    created tests for register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,40 @@
-// Write your tests here
+const server = require("./server");
+const request = require("supertest");
+const db = require("../data/dbConfig");
+
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
+});
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe("[POST] /api/auth/register", () => {
+  test("responds with status 201 and newly created user", async () => {
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "rose", password: "1234" });
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject({
+      id: 1,
+      username: "rose",
+      password: res.body.password,
+    });
+  });
+
+  test("responds with status 400 and error if username or password is missing", async () => {
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "" });
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({
+      message: "username and password required",
+    });
+  });
 });
